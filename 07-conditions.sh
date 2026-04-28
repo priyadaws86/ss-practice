ccfe2ac700ea1f0841610e9f26cb8ef780e64caa
priya <priyadaws86@gmail.com>
2026-04-28 12:37:43 +0530

    updated

--- 07-conditions.sh
+++ 07-conditions.sh
@@ -4,7 +4,7 @@
 NUMBER=$1
 
 if [ $NUMBER -lt 10 ]; then
-    echo "Given number $NUMBER is: Less than 10"
+    echo "Given number $NUMBER is Less than 10"
 else
-    echo "Given number $NUMBER is: Greater than 10"
+    echo "Given number $NUMBER is Greater than 10"
 fi  
