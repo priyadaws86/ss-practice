d56ae218f7e00d230587ae5865132677dcf5c8f2
priya <priyadaws86@gmail.com>
2026-04-28 11:56:44 +0530

    updated

---
+++ 02-Variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+PERSON1="Dev"
+PERSON2="Priya"
+echo "Hi $PERSON2, How are you doing?"
+echo "Hi $PERSON1, I am doing good, how about you?"
+echo "Hi $PERSON2, I am fine, thanks for asking."
+
