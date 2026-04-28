6550522a3401ee2b44c4409fc0c966d3c932e1db
priya <priyadaws86@gmail.com>
2026-04-28 12:52:22 +0530

    updated

--- 07-conditions.sh
+++ 07-conditions.sh
@@ -3,10 +3,10 @@
 
 NUMBER=$1
 
-if [ $NUMBER -lt 10 ];  then
-    echo "Given number $NUMBER is Less than 10"
-elif [ $NUMBER -eq 10 ]; then
-    echo "Given number $NUMBER is Equal to 10"    
+if [ $1 -lt 10 ];  then
+    echo "Given number $1 is Less than 10"
+elif [ $1 -eq 10 ]; then
+    echo "Given number $1 is Equal to 10"    
 else
-    echo "Given number $NUMBER is Greater than 10"
+    echo "Given number $1 is Greater than 10"
 fi  
