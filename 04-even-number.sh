d221dd231b0280d9c1053b4c5b4d23d495ad2e6f
priya <priyadaws86@gmail.com>
2026-04-28 12:59:03 +0530

    updated

--- 04-even-number.sh
+++ 04-even-number.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 #Print even number
 
+echo "Please enter a number"
+
 read NUMBER
 
-if [ $((NUMBER % 2)) -eq 0 ]
-then
+if [ $((NUMBER % 2)) -eq 0 ]; then
     echo "Given number $NUMBER is even"
     else
     echo "Given number $NUMBER is odd"
