494f40fa8060d74e9acb5b3dbf367bb0f2307323
priya <priyadaws86@gmail.com>
2026-04-28 12:48:24 +0530

    updated

--- 07-conditions.sh
+++ 07-conditions.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #Greater than and Less than
 
-NUMBER=$1
+NUMBER=12
 
-if [ "$1" -lt 10 ];  then
-    echo "Given number $1 is Less than 10"
+if [ "$NUMBER" -lt 10 ];  then
+    echo "Given number $NUMBER is Less than 10"
 else
-    echo "Given number $1 is Greater than 10"
+    echo "Given number $NUMBER is Greater than 10"
 fi  
