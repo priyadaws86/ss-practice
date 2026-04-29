c5c50818609f0fdd33a706e34e171a58df7b7c91
priya <priyadaws86@gmail.com>
2026-04-29 11:10:49 +0530

    Upadted

---
+++ 13.loops-log.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+LOGS_FOLDER="/var/log/shell-script"
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+
+mkdir -p $LOGS_FOLDER
+echo "Script started executed at : $(date)" | tee -a $LOG_FILE
+
+if [ $USERID -ne 0 ]; then
+    echo -e "$R Error:: Please run this script with root privilages $N"
+    exit 1
+fi  
+
+VALIDATE(){
+
+    if [ $1 -ne 0 ]; then
+        echo -e "$R ERROR: $2 Installation failed $N" | tee -a $LOG_FILE
+        exit 1
+    else
+        echo -e "$G $2 Installation successful $N" | tee -a $LOG_FILE
+    fi
+}
+
+# $@
+
+ for package in $@
+    do
+        echo " Package is : $package"
+    done    
